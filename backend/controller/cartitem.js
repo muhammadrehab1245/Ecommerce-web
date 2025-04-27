5c1438c13ac1da711a09cb4e27ab4b42a201f162
Muhammad Rehab <rehab123335@gmail.com>
2025-04-28 01:45:43 +0500

    'cart apis done'

--- backend/controller/cartitem.js
+++ backend/controller/cartitem.js
@@ -9,6 +9,18 @@ exports.FetchCart = async (req, res) => {
       }
 }
 
+exports.FetchCartById = async (req, res) => {
+  const { id } = req.params;
+  console.log(id)
+    try {
+      const cartitems = await Cartitems.findOne({ userId: id });
+      res.status(200).json(cartitems); // Send the products as a response
+    } catch (error) {
+      console.log('Error:', error);
+      res.status(500).json({ message: 'Internal Server Error' }); // Send an error response
+    }
+}
+
 exports.AddCart = async (req, res) => {
     const { itemname, tag, price, img, quantity,user } = req.body;
     console.log(user)
@@ -24,3 +36,40 @@ exports.AddCart = async (req, res) => {
       res.status(500).json({ message: 'Internal Server Error' });
     }
   };
+
+
+  exports.DeleteCart = async (req, res) => {
+    const { id } = req.body;
+  
+    try {
+      await Cartitems.deleteOne({id}); 
+      res.status(200).json('Cart Item Deleted Successfully');
+    } catch (error) {
+      console.error('Error adding item to cart:', error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  };
+
+  exports.DeleteCart = async (req, res) => {
+    const { id } = req.body;
+    console.log(id)
+    try {
+      await Cartitems.deleteOne({_id:id}); 
+      res.status(200).json('Cart Item Deleted Successfully');
+    } catch (error) {
+      console.error('Error adding item to cart:', error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  };
+
+  exports.EmptyingCart = async (req, res) => {
+    // const { id } = req.body;
+    
+    try {
+      await Cartitems.deleteMany({userId:req.user.id}); 
+      res.status(200).json('Cart Empty Successfully');
+    } catch (error) {
+      console.error('Error emptying item to cart:', error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  }
--- backend/route/cartitem.js
+++ backend/route/cartitem.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { FetchCart, AddCart } = require('../controller/cartitem');
+const { FetchCart, AddCart,FetchCartById,DeleteCart,EmptyingCart } = require('../controller/cartitem');
 const authMiddleware = require('../middleware/middleware')
     const router = express.Router();
-    router.get('/fetchcart',FetchCart)
+    router.get('/fetchcart',authMiddleware,FetchCart)
     router.post('/addcart',authMiddleware,AddCart)
+    router.get('/fetchcart/:id',authMiddleware,FetchCartById)
+    router.delete('/deletecart',authMiddleware,DeleteCart)
+    router.get('/emptycart',authMiddleware,EmptyingCart)
 
 module.exports = router;
